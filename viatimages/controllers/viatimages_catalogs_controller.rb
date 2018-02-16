81525e0d3c7b4ecd7e96b82451a6fffd453814f1
Luca Guindani <luca.guindani@gmail.com>
2018-02-16 11:17:08 +0100

    Added some logic in catalogs & items controllers Added some style for tag cloud & corpus Added some views to separate image and corpus items presentation

--- viatimages/controllers/viatimages_catalogs_controller.rb
+++ viatimages/controllers/viatimages_catalogs_controller.rb
@@ -1,13 +1,40 @@
 class ViatimagesCatalogsController < CatalogsController
   def show
-    @corpus_type = ItemType.where(catalog_id: @catalog.id).where(slug: 'corpus')
-    @corpus_title_field = @corpus_type.first.find_field('title')
-    @corpus_type_items = Item.where(item_type_id: @corpus_type.ids.first).sorted_by_field(@corpus_title_field)
+    image_type = ItemType.where(catalog_id: @catalog.id).where(slug: 'images')
 
-    @domain_choice_set = ChoiceSet.where(catalog_id: @catalog.id).where(name: 'Domaines')
-    @domain_choice_set_items = Choice.where(choice_set_id: @domain_choice_set.ids.first).sorted
+    # Retrieve & sort the all the corpuses
+    corpus_type = ItemType.where(catalog_id: @catalog.id).where(slug: 'corpus')
+    corpus_title_field = corpus_type.first.find_field('title')
+    @corpus_type_items = Item.where(item_type_id: corpus_type.ids.first).sorted_by_field(corpus_title_field)
 
-    @keyword_type = ItemType.where(catalog_id: @catalog.id).where(slug: 'keywords')
-    @keyword_type_items = Item.where(item_type_id: @keyword_type.ids.first)
+    # Retrieve & sort the all the domains
+    domain_choice_set = ChoiceSet.where(catalog_id: @catalog.id).where(name: 'Domaines')
+    @domain_choice_set_items = Choice.where(choice_set_id: domain_choice_set.ids.first).sorted
+
+    # Retrieve & sort the first 20 most used keywords
+    image_type_keyword_field = image_type.first.find_field('keyword')
+    image_type_items_with_keywords = Item.where(item_type_id: image_type.ids.first).where("data ->> '#{image_type_keyword_field.uuid}' != '[]'")
+    keywords = Array.new
+    image_type_items_with_keywords.each do |a|
+      a.get_value('keyword').each do |b|
+        keywords.push(b.item_type.primary_text_field.raw_value(b))
+      end
+    end
+    # Count duplicates keywords
+    @keywords_count = keywords.inject({}) { |a,e| a[e] = (a[e] || 0) + 1; a }
+    # Sort keywords by count, keep first 20 and shuffle the array
+    @keywords_count = @keywords_count.sort_by(&:last).reverse!.first(20).shuffle
+    # Replace keyword count with css classes
+    @keywords_count = @keywords_count.map { |keyword, count|
+      if count >= 8
+        {keyword => 'largest'}
+      elsif count >= 4
+        {keyword => 'large'}
+      elsif count >= 2
+        {keyword => 'medium'}
+      else
+        {keyword => 'small'}
+      end
+    }.reduce(:merge)
   end
 end
--- viatimages/controllers/viatimages_items_controller.rb
+++ viatimages/controllers/viatimages_items_controller.rb
@@ -1,8 +1,26 @@
 class ViatimagesItemsController < ItemsController
   def index
     super
+    # If corpus id request parameter is available, send corresponding corpus item to view
+    if params['corpus'].present?
+      corpus_id = params['corpus']
+      @corpus = Item.where(id: corpus_id).first
+    end
   end
   def show
     super
+    @corpus_item_type_slug = "corpus"
+    @image_item_type_image_slug = "images"
+
+    # Prepare some objects for the view according to the item type slug
+    if @item_type.slug == @corpus_item_type_slug
+      @item.applicable_fields.each do |field|
+        @lieu_edition = field if field.slug == "lieu-edition"
+        @tome_volume = field if field.slug == "tome-volume"
+        @langue_ouvrage = field if field.slug == "langue-ouvrage"
+        @collection_ouvrage = field if field.slug == "collection-ouvrage"
+      end
+      @etablissement = @item.get_value('etablissement')
+    end
   end
 end
