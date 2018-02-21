92018726b497654570469f086e33c29f61f689d9
Luca Guindani <luca.guindani@gmail.com>
2018-02-21 08:24:48 +0100

    Check for nil objects on catalog main page

--- viatimages/controllers/viatimages_catalogs_controller.rb
+++ viatimages/controllers/viatimages_catalogs_controller.rb
@@ -4,16 +4,18 @@ class ViatimagesCatalogsController < CatalogsController
 
     # Retrieve & sort the all the corpuses
     corpus_type = ItemType.where(catalog_id: @catalog.id).where(slug: 'corpus')
+    @corpus_type_items = nil if corpus_type.empty?
     corpus_title_field = corpus_type.first.find_field('title')
     @corpus_type_items = Item.where(item_type_id: corpus_type.ids.first).sorted_by_field(corpus_title_field)
 
     # Retrieve & sort the all the domains
     domain_choice_set = ChoiceSet.where(catalog_id: @catalog.id).where(name: 'Domaines')
+    @domain_choice_set_items = nil if domain_choice_set.empty?
     @domain_choice_set_items = Choice.where(choice_set_id: domain_choice_set.ids.first).sorted
 
     # Retrieve & sort the first 20 most used keywords
-    image_type_keyword_field = image_type.first.find_field('keyword')
-    image_type_items_with_keywords = Item.where(item_type_id: image_type.ids.first).where("data ->> '#{image_type_keyword_field.uuid}' != '[]'")
+    image_type_keyword_field = image_type.present? ? image_type.first.find_field('keyword') : nil
+    image_type_items_with_keywords = image_type_keyword_field.present? ? Item.where(item_type_id: image_type.ids.first).where("data ->> '#{image_type_keyword_field.uuid}' != '[]'") : []
     keywords = Array.new
     image_type_items_with_keywords.each do |a|
       a.get_value('keyword').each do |b|
