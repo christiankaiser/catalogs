db78bf4ceca2e67021a218c612705ed3db0885a9
Luca Guindani <luca.guindani@gmail.com>
2018-02-22 09:53:02 +0100

    Made some changes in controllers & views for corpus item type

--- viatimages/controllers/viatimages_items_controller.rb
+++ viatimages/controllers/viatimages_items_controller.rb
@@ -1,27 +1,33 @@
 class ViatimagesItemsController < ItemsController
+  include FieldsHelper
+
   def index
     super
+
     # If corpus id request parameter is available, send corresponding corpus item to view
-    if params['corpus'].present?
-      corpus_id = params['corpus']
-      @corpus = Item.where(id: corpus_id).first
-    end
+    @corpus = Item.where(id: params['corpus']).first if params['corpus'].present?
   end
   def show
     super
+
     @corpus_item_type_slug = "corpus"
     @image_item_type_image_slug = "images"
 
     # Prepare some objects for the view according to the item type slug
-    if @item_type.slug == @corpus_item_type_slug
-      @item.applicable_fields.each do |field|
-        @lieu_edition = field if field.slug == "lieu-edition"
-        @tome_volume = field if field.slug == "tome-volume"
-        @langue_ouvrage = field if field.slug == "langue-ouvrage"
-        @collection_ouvrage = field if field.slug == "collection-ouvrage"
-        @autre_editions = field if field.slug == "autres-editions"
-      end
-      @etablissement = @item.get_value('etablissement')
+    case @item_type.slug
+      when @corpus_item_type_slug
+        # objects for the "corpus" item type
+        @item.applicable_fields.each do |field|
+          @lieu_edition = field if field.slug == "lieu-edition"
+          @tome_volume = field if field.slug == "tome-volume"
+          @langue_ouvrage = field if field.slug == "langue-ouvrage"
+          @collection_ouvrage = field if field.slug == "collection-ouvrage"
+          @autre_editions = field if field.slug == "autres-editions"
+        end
+        @etablissement = @item.get_value('etablissement')
+        fields_and_item_references(@item) do |field, browse| @images_count = browse.total_count end
+      when @image_item_type_image_slug
+        # objects for the "images" item type
     end
   end
 end
